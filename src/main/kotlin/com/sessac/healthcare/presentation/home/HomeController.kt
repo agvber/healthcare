538991db556a2a9f11a29ef56dbc72ac5ba08f28
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-05-18 22:45:53 +0900

    chore: 주석 추가

--- src/main/kotlin/com/sessac/healthcare/presentation/home/HomeController.kt
+++ src/main/kotlin/com/sessac/healthcare/presentation/home/HomeController.kt
@@ -7,45 +7,38 @@ import com.sessac.healthcare.presentation.home.ui.HomeView
 
 class HomeController(
     private val user: UserDataModel,
-    private val histories: List<HistoryDataModel>
+    private val histories: List<HistoryDataModel>,
 ) {
     private lateinit var homeUIModel: HomeUIModel
     private var homeView = HomeView()
 
-    fun startHome(){
+    fun startHome() {
         homeUIModel = HomeUIMapper.mapToHomeUIModel(user, histories)
 
-        while(true) {
+        while (true) {
             homeView.displayHomeHeader()
             homeView.displayUserInfo(homeUIModel)
             homeView.displayDistanceInfo(homeUIModel)
             homeView.printTreeStatus(homeUIModel)
 
-            when(homeView.displayMenu().trim()) {
-                "1" -> {
-                    println("기록 추가")
-                    // TODO: 입력 기능 추가 되면 넣음.
-                }
-                "2" -> {
-                    println("기록 확인")
-                    // TODO: 입력 기능 추가 되면 넣음.
-                }
-                "3" -> {
-                    println("기록 수정")
-                    // TODO: 입력 기능 추가 되면 넣음.
-                }
-                "4" -> {
-                    println("기록 삭제")
-                    // TODO: 입력 기능 추가 되면 넣음.
-                }
-                "5" -> {
-                    println("내 정보")
-                    // TODO: 입력 기능 추가 되면 넣음.
-                }
+            when (homeView.displayMenu().trim()) {
+                "1" -> println("기록 추가") // TODO: 입력 기능 추가 되면 넣음.
+
+                "2" -> println("기록 확인") // TODO: 입력 기능 추가 되면 넣음.
+
+                "3" -> println("기록 수정") // TODO: 입력 기능 추가 되면 넣음.
+
+
+                "4" -> println("기록 삭제") // TODO: 입력 기능 추가 되면 넣음.
+
+
+                "5" -> println("내 정보") // TODO: 입력 기능 추가 되면 넣음.
+
                 "exit" -> {
                     println("프로그램을 종료합니다.")
                     return
                 }
+
                 else -> println("잘못된 입력입니다. 다시 입력하세요.")
             }
         }
