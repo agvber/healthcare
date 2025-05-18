97c37f77a5706d14d5a0bc80833ca448e44fb864
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-05-18 22:34:57 +0900

    feat: 트리 성장 과정 enum으로 추가

---
+++ src/main/kotlin/com/sessac/healthcare/presentation/home/model/TreeGrowthStage.kt
@@ -0,0 +1,15 @@
+package com.sessac.healthcare.presentation.home.model
+
+/**
+ * @property SPROUT 새싹 1단계
+ * @property SAPLING 어린 2단계
+ * @property MATURE_TREE 성숙한 3단계
+ * @property FULL_TREE 완전한 4단계
+ */
+
+enum class TreeGrowthStage {
+    SPROUT,
+    SAPLING,
+    MATURE_TREE,
+    FULL_TREE
+}
