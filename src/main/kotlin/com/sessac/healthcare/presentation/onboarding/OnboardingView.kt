4e36a9a9a141752a0c2dfdf1582b808a062df6bc
agvber <agvber12@gmail.com>
2025-05-20 19:02:09 +0900

    feat: 회원가입 과정에서 회원 정보를 받는 UI 수정

--- src/main/kotlin/com/sessac/healthcare/presentation/onboarding/OnboardingView.kt
+++ src/main/kotlin/com/sessac/healthcare/presentation/onboarding/OnboardingView.kt
@@ -8,7 +8,7 @@ class OnboardingView {
 
     fun inputUserInformation(): String {
         println("회원가입에 필요한 정보는 다음과 같습니다.")
-        println("아이디, 비밀번호, 닉네임, 키(cm), 체중(kg) 순으로 입력하여주세요.")
+        println("아이디(특수문자 X), 비밀번호(4 ~ 16자리, 특수문자 X), 닉네임(4 ~ 8자리, 특수문자 X), 키(cm), 체중(kg) 순으로 입력하여주세요.")
         print("입력): ")
         return readLine() ?: ""
     }
