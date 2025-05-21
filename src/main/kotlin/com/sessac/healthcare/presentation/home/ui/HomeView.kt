e49a29009608c1c4ec76105483bcc79d35f7cc84
BossMG-github <sonmingyu23@naver.com>
2025-05-21 16:43:16 +0900

    feat: 필요 없는 메뉴 삭제 및 건강 정보 추가

--- src/main/kotlin/com/sessac/healthcare/presentation/home/ui/HomeView.kt
+++ src/main/kotlin/com/sessac/healthcare/presentation/home/ui/HomeView.kt
@@ -1,12 +1,11 @@
 package com.sessac.healthcare.presentation.home.ui
 
 import com.sessac.healthcare.presentation.home.model.HomeUIModel
-import com.sessac.healthcare.presentation.home.model.TreeGrowthStage
 
-class HomeView {
+object HomeView {
 
     fun displayHomeHeader() {
-        println("======================================")
+        println("\n\n\n======================================")
         println(
             """
             *
@@ -19,19 +18,29 @@ class HomeView {
         """.trimIndent()
         )
         println("======================================")
+        println()
     }
 
     fun displayUserInfo(model: HomeUIModel) {
+        println("============= 사용자 정보 =============")
         println()
-        println("[ 사용자 이름: ${model.nickName} ]")
-        println("[ BMI: ${model.bmi} ]")
+        println("\t\t 사용자 이름  : ${model.nickName}")
+        println("\t\t 키         : ${model.height}cm")
+        println("\t\t 체중        : ${model.weight}kg")
+        println()
     }
 
     fun displayDistanceInfo(model: HomeUIModel) {
-        println("[ 일일 목표 거리: ${model.userGoalDistance}m ]")
-        println("[ 일일 누적 거리: ${model.userDailyDistance}m ] ")
-        println("[ 총 누적 거리: ${model.userTotalDistance}m ] ")
-        println("[ 건강을 위한 목표 거리: ${model.defaultGoalDistance}m ]")
+        println("=========== 거리 및 건강 정보 ===========")
+        println()
+        println("\t\t BMI          : ${model.bmi}")
+        println("\t\t 일일 목표 거리 : ${model.userGoalDistance}m")
+        println("\t\t 일일 누적 거리 : ${model.userDailyDistance}m")
+        println("\t\t 수명 연장 시간 : ${model.userDailyDistance}m")
+        println("\t\t 총 누적 거리   : ${model.userTotalDistance}m")
+        println("\t\t 건강을 위한 목표 거리 : ${model.defaultGoalDistance}m")
+        println("\t\t 늘어난 수명    : ${model.lifeExtension.first}시 ${model.lifeExtension.second}분")
+        println()
     }
 
     fun printTreeStatus(model: HomeUIModel) {
@@ -49,14 +58,12 @@ class HomeView {
     }
 
     fun displayMenu(): String{
-        println("=====================")
-        println("1. 기록 추가")
-        println("2. 기록 확인")
-        println("3. 기록 수정")
-        println("4. 기록 삭제")
-        println("5. 내 정보")
+        println("==================================")
+        println("1. 기록 화면")
+        println("2. 목표 화면")
+        println("3. 내 정보")
         println("exit. 프로그램 종료")
-        println("=====================\n")
+        println("==================================")
         print("선택 >> ")
         return readln()
     }
