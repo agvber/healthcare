40a9eabe3b4c330d94d0e9a1d1ae58cb29bab393
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-05-22 16:05:03 +0900

    feat: RecordMapper 작성

---
+++ src/main/kotlin/com/sessac/healthcare/presentation/record/RecordMapper.kt
@@ -0,0 +1,34 @@
+package com.sessac.healthcare.presentation.record
+
+import com.sessac.healthcare.data.model.HistoryDataModel
+import com.sessac.healthcare.data.utils.fileDateFormat
+import com.sessac.healthcare.data.utils.toLocalDateTime
+import java.time.Duration
+
+class RecordMapper {
+    fun historyDataModelToPresentation(history: HistoryDataModel): RecordPresentationModel {
+        val duration = Duration.between(history.startDateTime, history.endDateTime)
+        val hours = duration.toHours()
+        val minutes = duration.toMinutesPart()
+        val timeString = "${hours}시간 ${minutes}분"
+
+        return RecordPresentationModel(
+            userId = history.userId,
+            startDateTime = history.startDateTime.format(fileDateFormat),
+            time = timeString,
+            distanceWalked = history.distanceWalked,
+            memo = history.memo
+        )
+    }
+        fun stringToHistoryDataModel(value: String, userId: Long, lastId: Long): HistoryDataModel {
+        val record = value.split(",")
+        return HistoryDataModel(
+            userId = userId,
+            startDateTime = record[0].trim().toLocalDateTime(),
+            endDateTime = record[1].trim().toLocalDateTime(),
+            id = lastId + 1,
+            distanceWalked = record[2].trim().toLong(),
+            memo = record[3].trim()
+        )
+    }
+}
