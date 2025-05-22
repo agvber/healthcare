4f0f69a34ba3ab40cd931cdbe6e0ee3d8ce5b705
agvber <agvber12@gmail.com>
2025-05-22 23:05:17 +0900

    feat: 총걸은 거리를 가져오는 usecase 추가

---
+++ src/test/kotlin/com/sessac/healthcare/domain/usecase/GetTotalWalkedDistanceUseCaseUnitTest.kt
@@ -0,0 +1,53 @@
+package com.sessac.healthcare.domain.usecase
+
+import com.sessac.healthcare.data.datasource.HistoryDataSource
+import com.sessac.healthcare.data.model.HistoryDataModel
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class GetTotalWalkedDistanceUseCaseUnitTest {
+
+    private lateinit var getTotalWalkedDistanceUseCase: GetTotalWalkedDistanceUseCase
+
+    @BeforeEach
+    fun init() {
+        getTotalWalkedDistanceUseCase = GetTotalWalkedDistanceUseCase(
+            historyDataSource = object : HistoryDataSource {
+                override fun getUserHistories(userId: Long): List<HistoryDataModel> {
+                    val dummy = HistoryDataModel(
+                        id = 11,
+                        userId = 111,
+                        startDateTime = LocalDateTime.now().minusDays(11),
+                        endDateTime = LocalDateTime.now().minusDays(11).plusMinutes(45),
+                        memo = "체력 단련을 위한 조깅",
+                        distanceWalked = 8000
+                    )
+                    return listOf(dummy, dummy)
+                }
+
+                override fun getUserHistoriesByPeriod(
+                    userId: Long,
+                    startDate: LocalDate,
+                    endDate: LocalDate
+                ): List<HistoryDataModel> {
+                    TODO("Not yet implemented")
+                }
+
+                override fun setUserHistory(historyDataModel: HistoryDataModel) {
+                    TODO("Not yet implemented")
+                }
+
+            }
+        )
+    }
+
+    @Test
+    fun testUseCase() {
+        val totalWalkedDistance = getTotalWalkedDistanceUseCase()
+        Assertions.assertEquals(16000, totalWalkedDistance)
+    }
+
+}
