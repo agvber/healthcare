89bc812c3c9b1c21b1df956876342f35a014df20
agvber <agvber12@gmail.com>
2025-05-22 23:06:48 +0900

    feat: 걸음 거리를 계산하여 탄소 저감량을 계산하는 usecase 추가

---
+++ src/main/kotlin/com/sessac/healthcare/domain/usecase/GetUserTotalCarbonReductionUseCase.kt
@@ -0,0 +1,16 @@
+package com.sessac.healthcare.domain.usecase
+
+import com.sessac.healthcare.data.datasource.HistoryDataSource
+import com.sessac.healthcare.data.datasource.impl.HistoryDataSourceImpl
+import com.sessac.healthcare.domain.entites.ReportEntity
+
+class GetUserTotalCarbonReductionUseCase(
+    private val historyDataSource: HistoryDataSource = HistoryDataSourceImpl,
+    private val reportEntity: ReportEntity = ReportEntity()
+) {
+    operator fun invoke(): Double {
+        val userHistories = historyDataSource.getUserHistories(0) // 내 정보 가져오기
+        val totalDistanceWalked = userHistories.sumOf { it.distanceWalked }
+        return reportEntity.calculateCarbonReduction(totalDistanceWalked)
+    }
+}
