622b1edc2866bc5ff3bc93debbe2b08d735a1e7b
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-05-25 01:59:20 +0900

    refactor: HomeController 화면 연결 임시 추가

--- src/main/kotlin/com/sessac/healthcare/presentation/home/controller/HomeController.kt
+++ src/main/kotlin/com/sessac/healthcare/presentation/home/controller/HomeController.kt
@@ -2,9 +2,16 @@ package com.sessac.healthcare.presentation.home.controller
 
 import com.sessac.healthcare.data.model.HistoryDataModel
 import com.sessac.healthcare.data.model.UserDataModel
+import com.sessac.healthcare.presentation.goal.GoalController
 import com.sessac.healthcare.presentation.home.HomeUIMapper
 import com.sessac.healthcare.presentation.home.model.HomeUIModel
 import com.sessac.healthcare.presentation.home.ui.HomeView
+import com.sessac.healthcare.presentation.login.LoginController
+import com.sessac.healthcare.presentation.onboarding.OnboardingController
+import com.sessac.healthcare.presentation.record.RecordController
+import com.sessac.healthcare.presentation.report.ReportController
+import com.sessac.healthcare.presentation.userprofile.controller.UserProfileController
+import kotlin.system.exitProcess
 
 class HomeController(
     private val user: UserDataModel,
@@ -19,15 +26,15 @@ class HomeController(
         HomeView.displayUserInfo(homeUIModel)
         HomeView.displayDistanceInfo(homeUIModel)
 
-        when (val menu = HomeView.displayMenu().trim()) {
-            "1" -> TODO()
-            "2" -> TODO()
-            "3" -> TODO()
-            "exit" -> {
-                TODO()
-                System.exit(0)
-            }
-            else -> TODO()
+        when (val menu = HomeView.displayMenu().trim()) { // 1. 온보딩 2. 로그인 3. 기록 4. 내 정보 5. 리포트
+            "1" -> OnboardingController().run()
+            "2" -> LoginController().run()
+            "3" -> RecordController(user).run()
+            "4" -> UserProfileController(user).launchUserProfile()
+            "5" -> ReportController().run()
+            "6" -> GoalController().run()
+            "exit" -> exitProcess(0)
+            else -> TODO("잘못 입력하셨습니다.")
         }
 //
 //        when (val menu = HomeView.displayMenu().trim()) {
