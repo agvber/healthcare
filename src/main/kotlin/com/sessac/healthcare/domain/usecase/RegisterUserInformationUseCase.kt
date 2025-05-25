a75476d6d9807196f38cbf83ba71783f7e6c75cb
agvber <agvber12@gmail.com>
2025-05-25 23:46:17 +0900

    feat: 유저 정보를 처음 등록하는 usecase 추가

---
+++ src/main/kotlin/com/sessac/healthcare/domain/usecase/RegisterUserInformationUseCase.kt
@@ -0,0 +1,36 @@
+package com.sessac.healthcare.domain.usecase
+
+import com.sessac.healthcare.data.datasource.UserDataSource
+import com.sessac.healthcare.data.datasource.impl.UserDataSourceImpl
+import com.sessac.healthcare.data.model.UserDataModel
+import com.sessac.healthcare.domain.UserEntity
+import kotlin.random.Random
+
+class RegisterUserInformationUseCase(
+    private val userEntity: UserEntity = UserEntity(),
+    private val userDataSource: UserDataSource = UserDataSourceImpl
+) {
+
+    operator fun invoke(
+        id: String,
+        password: String,
+        nickname: String,
+        height: Float,
+        weight: Float
+    ) {
+        require(
+            userEntity.checkId(id) && userEntity.checkPassword(password) &&
+                    userEntity.checkNickname(nickname)
+        )
+
+        val userDataModel = UserDataModel(
+            id = Random.nextLong(10000, 9999999),
+            nickname = nickname,
+            height = userEntity.removeSecondFloatPlace(height).toInt(),
+            weight = userEntity.removeSecondFloatPlace(weight).toInt(),
+            goalDistance = 1000
+        )
+
+        userDataSource.setUser(userDataModel)
+    }
+}
