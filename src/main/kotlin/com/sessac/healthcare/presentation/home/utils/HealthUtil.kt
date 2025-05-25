78f59c499d1927d510af546fbd8d207836b63ee7
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-05-25 17:33:12 +0900

    refactor: 키, 체중 타입을 Float으로 변경 및 기본 목표 거리 설정하는 함수를 다른 파일로 옮김.

--- src/main/kotlin/com/sessac/healthcare/presentation/home/utils/HealthUtil.kt
+++ src/main/kotlin/com/sessac/healthcare/presentation/home/utils/HealthUtil.kt
@@ -9,27 +9,10 @@ object HealthUtil {
      *
      * @return 체중 / 키(m)^2
      */
-    fun calculateBMI(heightCm: Int, weightKg: Int): Float {
+    fun calculateBMI(heightCm: Float, weightKg: Float): Float {
         val heightMeter = heightCm / 100.0
         return (weightKg / (heightMeter * heightMeter)).toFloat()
     }
-
-    /**
-     * 기본 목표 거리 설정
-     * bmi 계산 없이 바로 사용 가능
-     * @return 저체중: 100km 정상: 150km 과체중: 200km 비만: 250km
-     */
-    fun calculateTotalGoalDistance(heightCm: Int, weightKg: Int): Long {
-        val bmi = calculateBMI(heightCm, weightKg)
-
-        return when {
-            bmi < 18.5 -> 100_000L  // 100 km
-            bmi < 25.0 -> 150_000L  // 150 km
-            bmi < 30.0 -> 200_000L  // 200 km
-            else -> 250_000L        // 250 km 이상
-        }
-    }
-
 
     /**
      * 누적 거리 비율에 따른 트리
