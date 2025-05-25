658f388d7dca5a30ec418607c70eb25f931160ed
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-05-25 16:08:43 +0900

    feat: GoalController 작성

---
+++ src/main/kotlin/com/sessac/healthcare/presentation/goal/GoalController.kt
@@ -0,0 +1,65 @@
+package com.sessac.healthcare.presentation.goal
+
+import com.sessac.healthcare.data.datasource.impl.UserDataSourceImpl
+import com.sessac.healthcare.data.model.HistoryDataModel
+import com.sessac.healthcare.data.model.UserDataModel
+import com.sessac.healthcare.presentation.common.ViewController
+
+class GoalController(private val user: UserDataModel, private val userRecords: List<HistoryDataModel>) : ViewController {
+    private lateinit var goalView: GoalView
+    private lateinit var goalMapper: GoalMapper
+    private lateinit var presentationModel: GoalPresentationModel
+
+    override fun run() {
+        goalMapper = GoalMapper()
+        presentationModel = goalMapper.dataModelToPresentation(user, userRecords)
+        goalView = GoalView()
+        showGoalScreen()
+        handleUserInput()
+    }
+
+    private fun showGoalScreen() {
+        goalView.printGoalDefaultMessage(user)
+        goalView.printUserTree(presentationModel.tree)
+        goalView.printGoal(user)
+        goalView.printRemainingBMIDistance(presentationModel.remainingBMIDistance)
+    }
+
+    private fun handleUserInput() {
+        when (goalView.askWantToInsertGoal()) {
+            "1" -> handleGoalInsertion()
+            "2" -> return // 이전 화면으로 이동
+            else -> goalView.printInvalidInputMessage()
+        }
+    }
+
+    private fun handleGoalInsertion() {
+        when (goalView.askGoalDistance()) {
+            "1" -> insertTotalGoalDistance()
+            "2" -> insertWeeklyGoalDistance()
+            "3" -> insertDailyGoalDistance()
+            else -> goalView.printInvalidInputMessage()
+        }
+    }
+
+    private fun insertTotalGoalDistance() {
+        val goalInput = goalView.inputTotalGoalDistance()
+        val updatedUser = UserDataSourceImpl.setUserGoalDistance(user.id, goalInput.toLong())
+        goalView.printUpdateGoal()
+        goalView.printGoal(updatedUser)
+    }
+
+    private fun insertWeeklyGoalDistance() {
+        val goalInput = goalView.inputWeeklyGoalDistance()
+        val updatedUser = UserDataSourceImpl.setUserWeeklyGoalDistance(user.id, goalInput.toLong())
+        goalView.printUpdateGoal()
+        goalView.printGoal(updatedUser)
+    }
+
+    private fun insertDailyGoalDistance() {
+        val goalInput = goalView.inputDailyGoalDistance()
+        val updatedUser = UserDataSourceImpl.setUserDailyGoalDistance(user.id, goalInput.toLong())
+        goalView.printUpdateGoal()
+        goalView.printGoal(updatedUser)
+    }
+}
