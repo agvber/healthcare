0ba7091f486d76a6d43803ea6b129e4f5782c10d
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-05-25 17:32:33 +0900

    refactor: 진입점에서 기록데이터를 전부 가져오지말고 해당 유저의 기록데이터를 가져오도록 수정

--- src/main/kotlin/com/sessac/healthcare/Entypoint.kt
+++ src/main/kotlin/com/sessac/healthcare/Entypoint.kt
@@ -1,5 +1,19 @@
 package com.sessac.healthcare
 
-fun main() {
+import com.sessac.healthcare.data.datasource.impl.HistoryDataSourceImpl
+import com.sessac.healthcare.data.model.fake.DummyHistoryModels
+import com.sessac.healthcare.data.model.fake.DummyUserModels
+import com.sessac.healthcare.presentation.home.controller.HomeController
 
+fun main() {
+    val user = DummyUserModels().get().first()
+    val histories = HistoryDataSourceImpl.getUserHistories(user.pk)
+
+
+    while (true) {
+        HomeController(
+            user = user,
+            histories = histories
+        ).launchHome()
+    }
 }
