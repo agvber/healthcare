b8a868548e3ee1f8a3085657d9e2bd128caab87d
agvber <agvber12@gmail.com>
2025-05-25 23:47:49 +0900

    feat: EntryPoint -> LoginController 수정

--- src/main/kotlin/com/sessac/healthcare/Entypoint.kt
+++ src/main/kotlin/com/sessac/healthcare/Entypoint.kt
@@ -1,14 +1,7 @@
 package com.sessac.healthcare
 
-import com.sessac.healthcare.data.model.fake.DummyHistoryModels
-import com.sessac.healthcare.data.model.fake.DummyUserModels
-import com.sessac.healthcare.presentation.home.controller.HomeController
+import com.sessac.healthcare.presentation.login.LoginController
 
 fun main() {
-    while (true) {
-        HomeController(
-            user = DummyUserModels().get()[0],
-            histories = DummyHistoryModels().get()
-        ).launchHome()
-    }
+    LoginController().run()
 }
