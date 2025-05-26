36b9912d31bc0af398a1241cb121df0195c1e4d9
agvber <agvber12@gmail.com>
2025-05-26 10:55:20 +0900

    refactor: Report design flow enhancement

--- src/main/kotlin/com/sessac/healthcare/presentation/report/ReportController.kt
+++ src/main/kotlin/com/sessac/healthcare/presentation/report/ReportController.kt
@@ -5,6 +5,7 @@ import com.sessac.healthcare.domain.usecase.GetUserInformationUseCase
 import com.sessac.healthcare.domain.usecase.GetUserTotalCarbonReductionUseCase
 import com.sessac.healthcare.domain.usecase.GetWeekStepCountUseCase
 import com.sessac.healthcare.presentation.common.ViewController
+import com.sessac.healthcare.presentation.common.loop
 
 class ReportController : ViewController {
 
@@ -20,9 +21,14 @@ class ReportController : ViewController {
         initProgram()
         initUseCase()
         loadInitData()
-        inputOptionNumber()
-        processMain()
+        loop({ isContinue() }) {
+            inputOptionNumber()
+            processMain()
+        }
     }
+
+    private fun isContinue(): Boolean =
+        reportPresentationModel.consoleMainOption != 5
 
     private fun initProgram() {
         reportView = ReportView()
@@ -45,11 +51,23 @@ class ReportController : ViewController {
     }
 
     private fun inputOptionNumber() {
-        while (true) {
-            reportView.inputOption()
-                ?.also { reportPresentationModel.consoleMainOption = it }
-                ?: reportView.printErrorMessage()
+        val option: Int? = reportView.inputOption()
+
+        if (option != null && option in mainOptionRange) {
+            reportPresentationModel.consoleMainOption = option
+            return
         }
+        inputRetryOptionNumber()
+    }
+
+    private fun inputRetryOptionNumber() {
+        val option: Int? = reportView.inputRetryOption()
+
+        if (option == null || option !in mainOptionRange) {
+            inputRetryOptionNumber()
+            return
+        }
+        reportPresentationModel.consoleMainOption = option
     }
 
     private fun processMain() {
@@ -101,5 +119,9 @@ class ReportController : ViewController {
             height = height,
             weight = weight
         )
+    }
+
+    companion object {
+        private val mainOptionRange: IntRange = 1..5
     }
 }
