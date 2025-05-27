aaee6cff1f9210cb00dac904a32f8e1ee9d3e0f6
agvber <agvber12@gmail.com>
2025-05-28 00:51:39 +0900

    feat: 유저 데이터 파일 입출력 기능 추가

---
+++ src/main/kotlin/com/sessac/healthcare/data/datasource/impl/FileUserDataSource.kt
@@ -0,0 +1,101 @@
+package com.sessac.healthcare.data.datasource.impl
+
+import com.sessac.healthcare.common.anontation.ExperimentalFeatureApi
+import com.sessac.healthcare.data.datasource.UserDataSource
+import com.sessac.healthcare.data.model.UserDataModel
+import com.sessac.healthcare.data.utils.FileParsingUtil
+import com.sessac.healthcare.data.utils.LogUtils
+import java.io.File
+import kotlin.io.path.createFile
+import kotlin.io.path.createParentDirectories
+import kotlin.io.path.notExists
+
+/**
+ * 운동 정보를 저장한 파일에 데이터를 가져오는 클래스입니다.
+ * 인스턴스를 직접 생성하지 말고 FileUserDataSource.getInstance() 함수를 호출해서 사용하세요!
+ *
+ * @property file 유저 파일
+ * @property fileParsingUtil 유저의 파일 텍스트 값을 파싱 Utils class
+ * @property userMap 유저의 데이터를 메모리 캐싱
+ * @constructor Create empty User file data source imp
+ */
+
+class FileUserDataSource(
+    private val file: File,
+    private val fileParsingUtil: FileParsingUtil,
+    private val userMap: LinkedHashMap<String, UserDataModel>
+) : UserDataSource {
+
+    init {
+        checkFileStatus()
+        loadData()
+    }
+
+    private fun checkFileStatus() {
+        val path = file.toPath()
+        if (path.notExists()) {
+            path.createParentDirectories()
+            path.createFile()
+        }
+    }
+
+    @OptIn(ExperimentalFeatureApi::class)
+    private fun loadData() {
+        file.useLines { sequence ->
+            sequence.forEach { userTextLine ->
+                if (!userTextLine.startsWith("<") || !userTextLine.endsWith("/>")) {
+                    LogUtils.printDebugLog(TAG, "file line error")
+                    return@forEach
+                }
+                val userDataModel = fileParsingUtil.parseToObj(userTextLine, UserDataModel::class)
+                userMap[userDataModel.userId] = userDataModel
+            }
+        }
+    }
+
+    fun saveData() {
+        file.bufferedWriter().use { bufferedWriter ->
+            userMap.forEach { (key, value) ->
+                val objText = fileParsingUtil.formatString(value)
+                bufferedWriter.write(objText)
+                bufferedWriter.newLine()
+            }
+        }
+    }
+
+    override fun getUsers(): List<UserDataModel> {
+        return userMap.values.toList()
+    }
+
+    override fun createUser(userDataModel: UserDataModel) {
+        require(userDataModel.userId !in userMap.keys) { "이미 존재하는 아이디입니다." }
+        userMap[userDataModel.userId] = userDataModel
+    }
+
+    override fun getUserById(id: String): UserDataModel {
+        return userMap[id] ?: throw IllegalArgumentException("존재하지 않는 아이디입니다.")
+    }
+
+    override fun updateUser(userDataModel: UserDataModel) {
+        userMap[userDataModel.userId] = userDataModel
+    }
+
+    override fun deleteUser(id: String) {
+        userMap.remove(id)
+    }
+
+    companion object {
+        const val TAG: String = "FileUserDataSource"
+        private const val PATH_NAME: String = "./data/files/user.txt"
+
+        private var instance: FileUserDataSource? = null
+
+        fun getInstance(): FileUserDataSource {
+            return instance ?: FileUserDataSource(
+                file = File(PATH_NAME),
+                fileParsingUtil = FileParsingUtil(),
+                userMap = LinkedHashMap()
+            ).also { instance = it }
+        }
+    }
+}
---
+++ src/test/kotlin/com/sessac/healthcare/data/datasource/FileUserDataSourceUnitTest.kt
@@ -0,0 +1,47 @@
+package com.sessac.healthcare.data.datasource
+
+import com.sessac.healthcare.data.datasource.impl.FileUserDataSource
+import com.sessac.healthcare.data.model.UserDataModel
+import com.sessac.healthcare.data.model.fake.DummyUserModels
+import com.sessac.healthcare.data.utils.FileParsingUtil
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.random.Random
+
+class FileUserDataSourceUnitTest {
+
+    @Test
+    fun `파일 읽기 쓰기 테스트`() {
+        val id = Random.nextInt(1000, 9999).toString()
+        val user = DummyUserModels().get()[0].copy(userId = id)
+        fileDataSource.createUser(user)
+        fileDataSource.saveData()
+
+        var lastLine = ""
+        file.useLines { it.forEach { lastLine = it } }
+
+        val expect = fileParsingUtil.formatString(user)
+        Assertions.assertEquals(expect, lastLine)
+    }
+
+
+    companion object {
+
+        private lateinit var file: File
+        private lateinit var fileParsingUtil: FileParsingUtil
+        private lateinit var userMemoryMap: LinkedHashMap<String, UserDataModel>
+        private lateinit var fileDataSource: FileUserDataSource
+
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            file = File("./data/test/user.txt")
+            fileParsingUtil = FileParsingUtil()
+            userMemoryMap = linkedMapOf()
+            fileDataSource = FileUserDataSource(file, FileParsingUtil(), userMemoryMap)
+        }
+
+    }
+}
