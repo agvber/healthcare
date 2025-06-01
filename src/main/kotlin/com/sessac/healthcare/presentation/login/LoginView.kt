3f8415a2da208fd1c467ea408ab2da5fce2640e1
agvber <agvber12@gmail.com>
2025-06-01 21:56:44 +0900

    refactor: LoginView 하드코딩된 문자열 제거

--- src/main/kotlin/com/sessac/healthcare/presentation/login/LoginView.kt
+++ src/main/kotlin/com/sessac/healthcare/presentation/login/LoginView.kt
@@ -3,32 +3,39 @@ package com.sessac.healthcare.presentation.login
 object LoginView {
 
     fun inputMainOption(): String? {
-        println(
-            """
-            안녕하세요. 헬스케어 콘솔 프로젝트에 오신 것을 환영합니다.
-            아래의 옵션을 선택하여 다음 화면을 진행하여주세요.
-            [1] 로그인 [2] 회원가입 [0] 프로그램 종료 
-        """.trimIndent()
-        )
-        print("입력): ")
+        println(StringResource.WELCOME_MESSAGE.trimIndent())
+        print(StringResource.INPUT_PROMPT)
         return readLine()
     }
 
     fun printOptionSelectError() {
-        println("해당 옵션은 존재하지 않습니다 다시입력하여주세요.")
+        println(StringResource.OPTION_ERROR)
     }
 
     fun inputId(): String? {
-        print("아이디를 입력하여주세요: ")
+        print(StringResource.ID_PROMPT)
         return readLine()
     }
 
     fun inputPassword(): String? {
-        print("비밀번호를 입력하여주세요: ")
+        print(StringResource.PASSWORD_PROMPT)
         return readLine()
     }
 
     fun printLoginErrorMessage() {
-        println("아이디 또는 비밀번호가 일치하지 않습니다.")
+        println(StringResource.LOGIN_ERROR)
     }
+}
+
+private object StringResource {
+    const val WELCOME_MESSAGE = """
+            안녕하세요. 헬스케어 콘솔 프로젝트에 오신 것을 환영합니다.
+            아래의 옵션을 선택하여 다음 화면을 진행하여주세요.
+            [1] 로그인 [2] 회원가입 [0] 프로그램 종료
+        """
+    const val OPTION_ERROR = "해당 옵션은 존재하지 않습니다 다시 입력하여 주세요."
+    const val LOGIN_ERROR = "아이디 또는 비밀번호가 일치하지 않습니다."
+    const val INPUT_PROMPT = "입력): "
+    const val ID_PROMPT = "아이디를 입력하여 주세요: "
+    const val PASSWORD_PROMPT = "비밀번호를 입력하여 주세요: "
 }
