cd95ba58a642ae2e1c7438013b0785dcb1a98ca3
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-06-02 03:00:33 +0900

    refactor: GoalController companion object 분리

---
+++ src/main/kotlin/com/sessac/healthcare/presentation/goal/constants/GoalControllerConstants.kt
@@ -0,0 +1,10 @@
+package com.sessac.healthcare.presentation.goal.constants
+
+object GoalControllerConstants {
+    const val INSERT_GOAL_NUMBER = "1"
+    const val BACK_NUMBER = "0"
+
+    const val TOTAL_GOAL_DISTANCE_NUMBER = "1"
+    const val WEEKLY_GOAL_DISTANCE_NUMBER = "2"
+    const val DAILY_GOAL_DISTANCE_NUMBER = "3"
+}
